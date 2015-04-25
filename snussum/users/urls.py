e0ded1199c91a1c7a839fe86ebcca451fd902a77
dobestan <dobestan@gmail.com>
2015-04-26 05:02:41 +0900

    add logout views and urls

--- snussum/users/urls.py
+++ snussum/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
 
     url(r'^login/$', 'users.views.login', name='login'),
     url(r'^signup/$', 'users.views.signup', name='signup'),
+    url(r'^logout/$', 'users.views.logout', name='logout'),
 )
