7f104502bb2b44c1d52d6a34637a980a914fc17d
dobestan <dobestan@gmail.com>
2015-04-29 00:49:24 +0900

    update view : very basic styles with flat colors

--- snussum/users/views/verify.py
+++ snussum/users/views/verify.py
@@ -4,10 +4,17 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 
 from django.contrib.auth.models import User
+from users.models.university import University
 
 
 class Verify(TemplateView):
     template_name = "users/verify.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Verify, self).get_context_data(**kwargs)
+        context["universities"] = University.objects.all()
+        return context
+
 
 
 def university_verification(request, university_verification_token):
