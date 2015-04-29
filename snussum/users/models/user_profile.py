8a054b913e200232a6c4a3e97133b4529db2f584
dobestan <dobestan@gmail.com>
2015-04-29 13:33:42 +0900

    update user_profile model : add profile_nickname, profile_introduce

--- snussum/users/models/user_profile.py
+++ snussum/users/models/user_profile.py
@@ -47,6 +47,16 @@ class UserProfile(models.Model):
     is_university_verified = models.BooleanField(default=False)
     university_verification_token = models.CharField(max_length=32, null=True, blank=True)
 
+    profile_nickname = models.CharField(max_length=8, blank=True, null=True, unique=True)
+    profile_introduce = models.TextField(blank=True, null=True)
+
+    def _is_profile_verified(self):
+        if not self.profile_nickname and \
+                not self.profile_introduce:
+            return False
+        return True
+    is_profile_verified = property(_is_profile_verified)
+
     def _is_girl(self):
         return not is_boy
     is_girl = property(_is_girl)
