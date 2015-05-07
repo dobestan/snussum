432150440512a0a1fc07364a545d8aab2c9c0b88
dobestan <dobestan@gmail.com>
2015-05-07 21:53:15 +0900

    fix pep8

--- snussum/users/decorators.py
+++ snussum/users/decorators.py
@@ -54,15 +54,15 @@ def university_not_verified_required(function=None):
     def _dec(view_func):
         def _view(request, *arg, **kwargs):
             if not request.user.userprofile.is_university_verified:
-                messages.add_message(request, messages.INFO,\
-                        '서비스 이용을 위해서 서울대학교 인증이 필요합니다.\
-                        현재 이메일 인증(마이스누), 로그인 인증(마이스누, 스누라이프)을 지원합니다.',\
-                        extra_tags="danger")
+                messages.add_message(request, messages.INFO,
+                                     '서비스 이용을 위해서 서울대학교 인증이 필요합니다.\
+                        현재 이메일 인증(마이스누), 로그인 인증(마이스누, 스누라이프)을 지원합니다.',
+                                     extra_tags="danger")
                 return view_func(request, *arg, **kwargs)
             else:
-                messages.add_message(request, messages.INFO,\
-                        '이미 대학교 인증이 완료되었습니다. ( %s )' % request.user.userprofile.university.full_name,\
-                        extra_tags="success")
+                messages.add_message(request, messages.INFO,
+                                     '이미 대학교 인증이 완료되었습니다. ( %s )' % request.user.userprofile.university.full_name,
+                                     extra_tags="success")
                 return redirect('users:verify-profile')
 
         _view.__name__ = view_func.__name__
@@ -83,10 +83,10 @@ def profile_verifed_required(function=None):
             if request.user.userprofile.is_profile_verified:
                 return view_func(request, *arg, **kwargs)
             else:
-                messages.add_message(request, messages.INFO,\
-                        '서비스 이용을 위해서 기본정보 입력이 필요합니다.\
-                        성실한 개인정보 입력은 상대방에 대한 예의입니다.',\
-                        extra_tags="danger")
+                messages.add_message(request, messages.INFO,
+                                     '서비스 이용을 위해서 기본정보 입력이 필요합니다.\
+                        성실한 개인정보 입력은 상대방에 대한 예의입니다.',
+                                     extra_tags="danger")
                 return redirect('users:verify-profile')
 
         _view.__name__ = view_func.__name__
