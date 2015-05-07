8486b5e9516fefb886e8732666305a245fe01e9a
dobestan <dobestan@gmail.com>
2015-05-07 19:31:49 +0900

    remove message on login

--- snussum/users/views/login.py
+++ snussum/users/views/login.py
@@ -5,9 +5,6 @@ from django.contrib import messages
 
 @anonymous_required
 def login(request):
-    messages.add_message(request, messages.INFO,\
-            '안전하게 로그인 되었습니다. 서비스를 이용해주셔서 감사합니다.',\
-            extra_tags="success")
     return default_login_view(
         request, template_name="users/login.html"
     )
