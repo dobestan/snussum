8281f260761baca5542d1a64e0e9ce230c15208d
dobestan <dobestan@gmail.com>
2015-05-07 19:23:51 +0900

    add custom messages on login, and logout

--- snussum/users/views/login.py
+++ snussum/users/views/login.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import login as default_login_view
-
 from users.decorators import anonymous_required
+from django.contrib import messages
 
 
 @anonymous_required
 def login(request):
+    messages.add_message(request, messages.INFO,\
+            '안전하게 로그인 되었습니다. 서비스를 이용해주셔서 감사합니다.',\
+            extra_tags="success")
     return default_login_view(
         request, template_name="users/login.html"
     )
--- snussum/users/views/logout.py
+++ snussum/users/views/logout.py
@@ -1,8 +1,11 @@
 from django.shortcuts import redirect
-
 from django.contrib.auth import logout as logout_user
+from django.contrib import messages
 
 
 def logout(request):
     logout_user(request)
+    messages.add_message(request, messages.INFO,\
+            '안전하게 로그아웃 되었습니다. 많은 서비스 이용 부탁드립니다. 감사합니다.',\
+            extra_tags="success")
     return redirect("home")
