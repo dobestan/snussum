76a58bcbaa41ed3fc6b3e4d70aa18356762c05c4
dobestan <dobestan@gmail.com>
2015-05-07 21:21:14 +0900

    profile verified user can access profile page

--- snussum/users/views/profile.py
+++ snussum/users/views/profile.py
@@ -1,11 +1,12 @@
 from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
-from users.decorators import university_verified_required
+from users.decorators import university_verified_required, profile_verifed_required
 
 
 class Profile(TemplateView):
     template_name = "users/profile.html"
 
     @method_decorator(university_verified_required)
+    @method_decorator(profile_verifed_required)
     def dispatch(self, *args, **kwargs):
         return super(Profile, self).dispatch(*args, **kwargs)
