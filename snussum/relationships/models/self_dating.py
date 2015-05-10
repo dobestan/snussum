409ef53728c0ecc97034f6b7b0a50b6be92a8fdb
dobestan <dobestan@gmail.com>
2015-05-11 00:26:40 +0900

    update SelfDatingModel : created_at, ends_at

--- snussum/relationships/models/self_dating.py
+++ snussum/relationships/models/self_dating.py
@@ -8,10 +8,18 @@ from relationships.utils.hashids import get_encoded_self_dating_hashid
 
 from django.contrib.auth.models import User
 
+from datetime import timedelta
+
 
 class SelfDating(models.Model):
     hash_id = models.CharField(max_length=8, unique=True, blank=True, null=True)
     user = models.ForeignKey(User)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    ends_at = models.DateTimeField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class SelfDatingApply(models.Model):
@@ -20,7 +28,11 @@ class SelfDatingApply(models.Model):
 
 
 @receiver(post_save, sender=SelfDating)
-def _update_self_dating_hash_id(sender, instance, created, **kwargs):
+def _update_self_dating(sender, instance, created, **kwargs):
     if created:
         instance.hash_id = get_encoded_self_dating_hashid(instance.id)
+
+        if not instance.ends_at:
+            instance.ends_at = instance.created_at + timedelta(3)
+
         instance.save()
