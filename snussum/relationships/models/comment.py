06df9f772acc7040847d1072060b5bf3d85499f9
dobestan <dobestan@gmail.com>
2015-05-10 18:59:27 +0900

    update DatingComment : add hash_id and custom url inside dating-detail

--- snussum/relationships/models/comment.py
+++ snussum/relationships/models/comment.py
@@ -3,12 +3,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from relationships.models.dating import Dating
 
+from relationships.utils.hashids import get_encoded_comment_hashid
+
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from notifications import notify
 
 
 class Comment(models.Model):
+    hash_id = models.CharField(max_length=16, unique=True, blank=True, null=True)
+
     user = models.ForeignKey(User)
     dating = models.ForeignKey(Dating)
 
@@ -20,6 +24,9 @@ class Comment(models.Model):
 @receiver(post_save, sender=Comment)
 def _send_notifications(sender, instance, created, **kwargs):
     if created:
+        instance.hash_id = get_encoded_comment_hashid(instance.id)
+        instance.save()
+
         if instance.user.userprofile.is_boy:
             partner = instance.dating.girl
         else:
