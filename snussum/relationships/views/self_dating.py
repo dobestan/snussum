b0fe05badff3767d1d0532f21f1e8e6043b5bcc3
dobestan <dobestan@gmail.com>
2015-05-11 15:43:14 +0900

    fix error : add name attr on input, so create valid data

--- snussum/relationships/views/self_dating.py
+++ snussum/relationships/views/self_dating.py
@@ -1,10 +1,17 @@
 from django.views.generic.edit import CreateView
 from relationships.forms.self_dating import SelfDatingForm
 
+from django.utils.decorators import method_decorator
+from users.decorators import university_verified_required, profile_verifed_required
 
 class SelfDatingFormView(CreateView):
     template_name = "datings/self_dating/new.html"
     form_class = SelfDatingForm
+
+    @method_decorator(university_verified_required)
+    @method_decorator(profile_verifed_required)
+    def dispatch(self, *args, **kwargs):
+        return super(SelfDatingFormView, self).dispatch(*args, **kwargs)
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
