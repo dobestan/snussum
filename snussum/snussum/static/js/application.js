8c3af9ef8eab98e7fbfc1e210c96844240fb8b41
dobestan <dobestan@gmail.com>
2015-05-12 23:05:03 +0900

    refactor javascript : enable smooth scrolling all over site

--- snussum/snussum/static/js/application.js
+++ snussum/snussum/static/js/application.js
@@ -31,4 +31,22 @@ $(document).ready(function(){
       {message: messages[i]}, {type: tags[i]}
     );
   }
+
+  // Smooth Scrolling
+  $('body').scrollspy({target: '#scrollspy'});
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 });
