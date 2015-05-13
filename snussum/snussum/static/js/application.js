522aea63304f91aab98b95713d2d489366491df7
dobestan <dobestan@gmail.com>
2015-05-13 12:41:04 +0900

    fix error : enable data-toggle tab ( bootstrap javascript tab ), error occured via smooth scrolling

--- snussum/snussum/static/js/application.js
+++ snussum/snussum/static/js/application.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
   $('body').scrollspy({target: '#scrollspy'});
 
   $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('a[href*=#]:not([href=#]):not([data-toggle])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html,body').animate({
             scrollTop: target.offset().top
-          }, 1000);
+          }, 500);
           return false;
         }
       }
