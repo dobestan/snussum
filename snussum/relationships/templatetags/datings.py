b408feb44d6815a5e4e30f8bcb3bdc43892326ac
dobestan <dobestan@gmail.com>
2015-05-13 23:14:06 +0900

    add templatetags : to return partner_is_accepted, myself_is_accepted

--- snussum/relationships/templatetags/datings.py
+++ snussum/relationships/templatetags/datings.py
@@ -6,10 +6,23 @@ register = template.Library()
 
 @register.filter(name='partner')
 def partner(dating, user):
-    if not dating:
-        return None
-
     if dating.boy == user:
         return dating.girl
     else:
         return dating.boy
+
+
+@register.filter(name="partner_is_accepted")
+def partner_is_accepted(dating, user):
+    if dating.boy == user:
+        return dating.is_girl_accepted
+    else:
+        return dating.is_boy_accepted
+
+
+@register.filter(name="myself_is_accepted")
+def myself_is_accepted(dating, user):
+    if dating.boy == user:
+        return dating.is_boy_accepted
+    else:
+        return dating.is_girl_accepted
