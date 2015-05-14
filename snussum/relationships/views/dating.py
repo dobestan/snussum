a0185767393462aa1d2ab90ee9f35dea08dc1fa0
dobestan <dobestan@gmail.com>
2015-05-14 22:01:43 +0900

    refactor datings : with DatingBase class and add login permission

--- snussum/relationships/views/dating.py
+++ snussum/relationships/views/dating.py
@@ -1,21 +1,26 @@
 from django.views.generic.detail import DetailView
 
 from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from users.decorators import university_verified_required, profile_verifed_required
 
 from relationships.models.dating import Dating
 
 
-class DatingDetail(DetailView):
+class DatingBase(DetailView):
     model = Dating
     slug_field = "hash_id"
-    template_name = "datings/detail.html"
     context_object_name = "dating"
 
+    @method_decorator(login_required)
     @method_decorator(university_verified_required)
     @method_decorator(profile_verifed_required)
     def dispatch(self, *args, **kwargs):
-        return super(DatingDetail, self).dispatch(*args, **kwargs)
+        return super(DatingBase, self).dispatch(*args, **kwargs)
+
+
+class DatingDetail(DatingBase):
+    template_name = "datings/detail.html"
 
 
 class TodayDetail(DatingDetail):
