4458dd1741688ebc0a73450d2690bc1cc4067e1e
dobestan <dobestan@gmail.com>
2015-05-17 21:06:26 +0900

    update users:verify:profile : add optional fields including age, height

--- snussum/users/views/verify.py
+++ snussum/users/views/verify.py
@@ -38,7 +38,7 @@ class VerifyUniversitySNU(TemplateView):
 class VerifyProfile(UpdateView):
     model = UserProfile
     template_name = "users/verify/profile.html"
-    fields = ['nickname', 'is_boy', 'profile_introduce', 'age', 'height', ]
+    fields = ['nickname', 'profile_introduce', 'age', 'height', ]
 
     @method_decorator(university_verified_required)
     def dispatch(self, *args, **kwargs):
