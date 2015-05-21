dbd6d8258accf3e0fe74f9f51248e53be8cabdcb
dobestan <dobestan@gmail.com>
2015-05-21 20:59:45 +0900

    update createdatings command : add descriptions for datings count

--- snussum/relationships/management/commands/createdatings.py
+++ snussum/relationships/management/commands/createdatings.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand, CommandError
 
 from relationships.models.dating import Dating
 from relationships.tasks.datings import match_all
+
+from datetime import datetime
 from datetime import timedelta
 
 
@@ -12,8 +14,13 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        for z in range(10):
+        for x in range(1, 10+1):
             match_all()
+            self.stdout.write(
+                'Day(%02d) > %03d datings matched, %03d datings total' %\
+                    (x, Dating.objects.filter(matched_at=datetime.today()).count(), Dating.objects.count())
+            )
+
             for dating in Dating.objects.all():
                 dating.matched_at -= timedelta(1)
                 dating.save()
