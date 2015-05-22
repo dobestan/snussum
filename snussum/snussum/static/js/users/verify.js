f321cb65f5de98825fcd42ae7201e7fe48d64bfc
dobestan <dobestan@gmail.com>
2015-05-22 14:26:50 +0900

    refactor javascripts on verify:profile

---
+++ snussum/snussum/static/js/users/verify.js
@@ -0,0 +1,53 @@
+(function(){
+  "use strict";
+  $(document).ready(function(){
+    var boy = $("section#verify-profile-gender").find("img#boy");
+    var girl = $("section#verify-profile-gender").find("img#girl");
+
+    var boy_makeup = $("section#verify-profile-gender").find("img#boy_makeup");
+    var girl_makeup = $("section#verify-profile-gender").find("img#girl_makeup");
+
+    var input_is_boy = $("section#verify-profile-gender").find("input#id_is_boy");
+
+    $(boy).click(function(){
+      $(girl_makeup).hide();
+      $(girl).show();
+
+      $(boy).hide();
+      $(boy_makeup).show();
+      $(boy_makeup).addClass("active");
+
+      $(input_is_boy).prop("value", "boy");
+    });
+
+    $(girl).click(function(){
+      $(boy_makeup).hide();
+      $(boy).show();
+
+      $(girl).hide();
+      $(girl_makeup).show();
+      $(girl_makeup).addClass("active");
+
+      $(input_is_boy).prop("value", "girl");
+    });
+
+    $("section#verify-profile-introduce").find("textarea#id_profile_introduce").on('input', function(){
+        var length = $(this).val().length;
+        var form = $(this).parent(".form-group");
+
+        var success_msg = $(form).find("p.success");
+        var danger_msg = $(form).find("p.danger");
+
+        if (length > 50) {
+          $(form).removeClass("border-danger").addClass("border-success");
+          $(danger_msg).hide();
+          $(success_msg).show();
+
+        } else {
+          $(form).removeClass("border-success").addClass("border-danger");
+          $(success_msg).hide();
+          $(danger_msg).show();
+        }
+    });
+  });
+})();
