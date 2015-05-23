94bdcd649fd280f8d55e523803eebbde525affab
dobestan <dobestan@gmail.com>
2015-05-23 15:33:51 +0900

    update snusex/_base.html : base html structure with header, footer

--- snussum/snusex/urls.py
+++ snussum/snusex/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from snusex.views import About
+from snusex.views import Home, About
 
 
 urlpatterns = patterns(
     '',
+    url(r'^$', Home.as_view(), name='home'),
     url(r'^about/$', About.as_view(), name='about'),
 )
