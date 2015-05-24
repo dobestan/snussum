25833491205966f2387c655ff3df7fafd2baec04
dobestan <dobestan@gmail.com>
2015-05-24 23:23:10 +0900

    update self-dating:new add content length verification

--- snussum/snussum/static/js/datings/self-datings.js
+++ snussum/snussum/static/js/datings/self-datings.js
@@ -13,4 +13,24 @@
       $(this).closest("li.self-dating-apply").find(".editing-refuse").show();
     });
   });
+
+  $(document).ready(function(){
+    $("section#self-dating-new").find("#id_content").on("summernote.change", function(){
+      var length = $(this).code().replace(/<(?:.|\n)*?>/gm, '').length;
+      var form = $(this).parent(".form-group");
+      var success_msg = $(form).find("p.success");
+      var danger_msg = $(form).find("p.danger");
+      var button = $(form).parent("form").find("button[type='submit']");
+
+      if (length > 200) {
+        $(danger_msg).hide();
+        $(success_msg).show();
+        $(button).prop("disabled", false);
+      } else {
+        $(success_msg).hide();
+        $(danger_msg).show();
+        $(button).prop("disabled", true);
+      }
+    });
+  });
 })();
