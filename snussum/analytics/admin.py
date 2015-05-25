db82cab10ae80e1125af93b9e47306a67a7b741f
dobestan <dobestan@gmail.com>
2015-05-25 17:08:21 +0900

    update Demographics Admin : include created_at field

--- snussum/analytics/admin.py
+++ snussum/analytics/admin.py
@@ -5,7 +5,7 @@ from analytics.models.demographics import Demographics
 
 @admin.register(Demographics)
 class DemographicsAdmin(admin.ModelAdmin):
-    readonly_fields = ('date', )
+    readonly_fields = ('date', 'created_at')
     list_display = admin.ModelAdmin.list_display + (
         'users', 'boys', 'girls',
         'users_university_verified', 'boys_university_verified', 'girls_university_verified',
