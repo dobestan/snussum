240c0e0339222712791abb5bfcd4a24549ae5000
dobestan <dobestan@gmail.com>
2015-05-25 16:19:56 +0900

    init Demographics Model : with Basic Demographics

--- snussum/analytics/models/demographics.py
+++ snussum/analytics/models/demographics.py
@@ -2,4 +2,21 @@ from django.db import models
 
 
 class Demographics(models.Model):
-    pass
+    # Basic Demographics
+    users = models.IntegerField(blank=True, null=True)
+    boys = models.IntegerField(blank=True, null=True)
+    girls = models.IntegerField(blank=True, null=True)
+
+    # Basic Demographics with University Verified
+    users_university_verified = models.IntegerField(blank=True, null=True)
+    boys_university_verified = models.IntegerField(blank=True, null=True)
+    girls_university_verified = models.IntegerField(blank=True, null=True)
+
+    # Basic Demographics with Profile Verified
+    # - including is_boy, nickname, profile_introduce
+    users_profile_verified = models.IntegerField(blank=True, null=True)
+    boys_profile_verified = models.IntegerField(blank=True, null=True)
+    girls_profile_verified = models.IntegerField(blank=True, null=True)
+
+    # Date
+    date = models.DateField(auto_now_add=True)
