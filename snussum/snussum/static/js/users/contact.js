5ba245d0223d6f1dc3833f6215825d167172d17d
dobestan <dobestan@gmail.com>
2015-05-27 20:08:09 +0900

    update contact js : only send data on contents valid

--- snussum/snussum/static/js/users/contact.js
+++ snussum/snussum/static/js/users/contact.js
@@ -14,25 +14,38 @@
       var content = $(contact_section).find("textarea#contact_content").val();
       var contact = $(contact_section).find("input#contact_contact").val();
 
-      var data = {
-        'content': content,
-        'contact': contact
-      };
+      if (content.length > 0){
+        var data = {
+          'content': content,
+          'contact': contact
+        };
 
-      $.ajax({
-        url: '/api/contact/',
-        type: 'POST',
-        data: data,
-        success: function(result){
-          $(contact_section).toggle();
-          $.notify({message: '성공적으로 피드백/문의사항이 전달되었습니다.'},
-            {type: "success"});
-        },
-        error: function(result){
-          $.notify({message: '현재 서버와의 통신에 문제가 있습니다.'},
-            {type: "danger"});
-        }
-      });
+        $.ajax({
+          url: '/api/contact/',
+          type: 'POST',
+          data: data,
+          success: function(result){
+            $(contact_section).toggle();
+            $.notify({message: '성공적으로 피드백/문의사항이 전달되었습니다.'},
+              {type: "success"});
+
+            $(contact_section).find("textarea#contact_content").val("");
+            $(contact_section).find("input#contact_contact").val("");
+          },
+          error: function(result){
+            $.notify({message: '현재 서버와의 통신에 문제가 있습니다.'},
+              {type: "danger"});
+          }
+        });
+      } else {
+        $.notify(
+          {
+            icon: "fa fa-exclamation-triangle",
+            message: "텍스트 입력 후 전송 부탁드립니다. 감사합니다."
+          },
+          {type: 'danger'}
+        );
+      }
     });
   });
 })();
